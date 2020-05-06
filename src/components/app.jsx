02800f97385a124f8ceaa28f8ba23a146de435f4
Karl Loomes <kloomes@hotmail.com>
2020-05-06 16:48:08 +0100

    Prior to Giphy API

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,34 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+
+import SearchBar from './search_bar';
+import Gif from './gif';
+import GifList from './gif_list';
 
 class App extends Component {
-  const constructor = (props) => {
-    super(props)
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      gifs: [],
+      selectedGifId: "LmNwrBhejkK9EFP504"
+    };
+  }
 
+  render() {
+    return (
+      <div>
+        <div className="left-scene">
+          <SearchBar />
+          <div className="selected-gif">
+            <Gif id={this.state.selectedGifId} />
+          </div>
+        </div>
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs} />
+        </div>
+      </div>
+    );
   }
 }
+
+export default App;
